4413624a6231f199e7947418054cd79b0f5b0e5e
melonfruity <eric.luo24@gmail.com>
2019-12-02 21:49:55 -0500

    got rid of all the rule violations I could. make the play function work and changed the colors of the blocks to better fit the game. Tried to learn and use flex box

--- index.js
+++ index.js
@@ -5,8 +5,6 @@ let playerTwo = {id: 'p2', deployedAllShips: false};
 playerOne.nextPlayer = playerTwo;
 playerTwo.nextPlayer = playerOne;
 
-const container = document.querySelector('#container');
-// const resetButton = document.querySelector('#reset');
 const playerOneBoard = document.querySelector('#playerOneBoard');
 const playerTwoBoard = document.querySelector('#playerTwoBoard');
 const columns = Array.from(document.querySelectorAll('.letters'));
@@ -22,15 +20,17 @@ const battleShipLengths = [5, 4, 3, 3, 2];
 const battleShipId = ['carrier', 'battleship', 'cruiser', 'submarine', 'destroyer'];
 
 // Ship deployment and game start flags
-let currentPlayer = playerOne; // Can random this
+let currentPlayer = Math.round(Math.random()) === 0 ? playerOne : playerTwo;
 let gameStarted = false; // Game started flag
+let gameEnd = false;
 let shipId = battleShipId[0]; // Initial ship
 let direction = true; // Current direction of a ship (vertical: true or horizontal: false)
 
 // Game play
 let crossHair = {
-  components: [`${Math.floor(dimensions/2)}${Math.floor(dimensions/2)}`]
+  components: [`${Math.floor(dimensions / 2)}${Math.floor(dimensions / 2)}`]
 };
+
 // Returns new coordinates depending on key code
 const newPos = {
   87: (x, y) => `${x}${Number(y) - 1}`,
@@ -42,12 +42,13 @@ const newPos = {
   75: (x, y) => `${x}${Number(y) + 1}`,
   76: (x, y) => `${Number(x) + 1}${y}`,
 };
+
 const validKeyCodes = {
   // Valid key codes to play with
   // w: 87  i: 73  up
   // a: 65  j: 74  left
   // s: 83  k: 75  down
-  // d: 68  l: 76  right 
+  // d: 68  l: 76  right
   // f: 70  r: 82 reset
   // enter: 13
   87: true,
@@ -57,51 +58,62 @@ const validKeyCodes = {
   13: true,
   70: true,
 };
+
 // Size of board
 // const createLettersArray use fromCharCode 65 - 91
 // Create the Grid, Tiles, and adding the tile objects for referencing to the player object
 const createGrid = (board, player) => {
   // Create the dimensions and border of a board
   board.style.cssText = `
-    display: grid; 
-    height: ${gridSize}px; 
-    width: ${gridSize}px; 
+    display: grid;
+    height: ${gridSize}px;
+    width: ${gridSize}px;
     border: 1px solid black;
     grid-template-columns: repeat(10, 1fr);
     grid-gap: 0`;
   // Creating the board property for player object
-  const boardObject = {}
+  const boardObject = {};
 
   // Creating the tile and referencing
-  for (let i = 0; i < 100; i++){
+  for (let i = 0; i < 100; i++) {
+    
     let columnIndex = i % 10;
-    let rowIndex = Math.floor( i / 10);
+    let rowIndex = Math.floor(i / 10);
     let id = `${letters[columnIndex]}${rowIndex + 1}`;
+    
     const gridItem = document.createElement('div');
-      gridItem.style.cssText = `border: 1px solid black`;
-      gridItem.id = id;
-      gridItem.className = `${player.id}tile`;
+    gridItem.style.cssText = `border: 1px solid black`;
+    gridItem.id = id;
+    gridItem.className = `${player.id}tile`;
+
     board.appendChild(gridItem);
     
     // Referencing
     boardObject[`${columnIndex}${rowIndex}`] = {
-      'tile': gridItem, 
+      'tile': gridItem,
       'id': `${letters[columnIndex]}${rowIndex + 1}`,
       'occupiedBy': false,
       'componentIndex': false,
+      'shotAt': false
     };
   }
+
   player.board = boardObject;
+
 };
+
 const coordinateContext = (columns, rows, letters) => {
+
   columns.forEach(column => {
+    
     column.style.cssText = `
-      display: grid; 
-      height: ${Math.sqrt(gridSize)}px; 
+      display: grid;
+      height: ${Math.sqrt(gridSize)}px;
       width: ${gridSize}px;
       grid-template-columns: repeat(10, 1fr);
       border: 1px solid black`;
-    for(let i = 0; i < letters.length; i ++){
+
+    for (let i = 0; i < letters.length; i ++) {
       const letter = document.createElement('div');
       letter.textContent = `${letters[i]}`;
       letter.style.cssText = `
@@ -110,17 +122,19 @@ const coordinateContext = (columns, rows, letters) => {
         vertical-align: middle`;
       column.appendChild(letter);
     }
+
   });
   
   rows.forEach(row => {
+
     row.style.cssText = `
-      display: grid; 
-      height: ${gridSize}px; 
+      display: grid;
+      height: ${gridSize}px;
       width: ${Math.sqrt(gridSize)}px;
       grid-template-rows: repeat(10, 1fr);
       border: 1px solid black`;
     
-    for(let i = 0; i < letters.length; i ++){
+    for (let i = 0; i < letters.length; i ++) {
       const number = document.createElement('div');
       number.textContent = `${i + 1}`;
       number.style.cssText = `
@@ -131,18 +145,19 @@ const coordinateContext = (columns, rows, letters) => {
     }
   });
 };
+
 // Make the ships
 const battleShipFactory = (player) => {
   
   const battleShipArray = battleShipLengths.reduce((ships, length, i) => {
     let battleShipObj = {
-      id: battleShipId[i], 
-      length: length, 
-      status: 'inactive', 
+      id: battleShipId[i],
+      length: length,
+      status: 'inactive',
       components: []};
     
     // Make the starting location of the ship at the top left of the board
-    for(let i = 0; i < length; i ++){
+    for (let i = 0; i < length; i ++) {
       battleShipObj.components.push(`0${i}`);
     }
     ships.push(battleShipObj);
@@ -151,35 +166,39 @@ const battleShipFactory = (player) => {
 
   const battleShips = battleShipArray.reduce((names, ship, i) => {
     names[battleShipId[i]] = ship;
-    return names;    
-  }, {}) 
+    return names;
+  }, {});
+
   player.shipsLeft = battleShipId.length;
   player.ships = battleShips;
 };
+
 // Deployment phase
 const moveShip = (player, shipId, keyCode) => {
   // Let deployShip function decide which ship is going to be moving
   let ship = player.ships[shipId];
   let comp = ship.components;
   
-  if(keyCode !== 13 && keyCode !== 70){
+  if (keyCode !== 13 && keyCode !== 70) {
     //If the ship is at the boundaries
-    if(!checkIfMoveable(ship, keyCode)) return;
+    if (!checkIfMoveable(ship, keyCode)) {
+      return;
+    }
 
     // Vertical fix and Horizontal fix 83 s 68 d
-    if((keyCode === 83 && direction) || (keyCode === 68)){
+    if ((keyCode === 83 && direction) || (keyCode === 68)) {
       player.ships[shipId].components = comp
         .reverse()
         .map(c => changeTile(c, keyCode, player))
-        .reverse()
+        .reverse();
     } else {
       player.ships[shipId].components = comp
         .map(c => changeTile(c, keyCode, player));
     }
-  } else if(keyCode === 13){
+  } else if (keyCode === 13) {
     // Deploys and maps a battleship
     deployShip(player, shipId, comp);
-  } else if(keyCode === 70){
+  } else if (keyCode === 70) {
     changeDirection(player, shipId, comp);
   } else {
     return;
@@ -187,29 +206,34 @@ const moveShip = (player, shipId, keyCode) => {
 };
 // Changes the color of the tile based on the occupancy
 const changeTile = (c, keyCode, player) => {
+
   const next = newPos[keyCode](c[0], c[1]);
   const curr = c;
-  if(!gameStarted){
-    if(player.board[curr].occupiedBy){
+  
+  if (!gameStarted) {
+    if (player.board[curr].occupiedBy) {
       player.board[curr].tile.style[`background-color`] = `brown`;
     } else {
       player.board[curr].tile.style[`background-color`] = `white`;
     }
     player.board[next].tile.style[`background-color`] = `black`;
     return next;
-  } 
-  const isItDamaged = player.board[curr].componentIndex;
+  }
 
-  if(!player.board[curr].occupiedBy || isItDamaged !== true){
-    player.board[curr].tile.style[`background-color`] = `blue`;
+  const isItDamaged = player.board[curr].componentIndex;
+  
+  if (!player.board[curr].occupiedBy || isItDamaged !== true) {
+    player.board[curr].tile.style[`background-color`] = player.board[curr].shotAt ? `blue` : `gray`;
   } else {
-    player.board[curr].tile.style[`background-color`] = isItDamaged === true ? `brown` : `blue`;
+    player.board[curr].tile.style[`background-color`] = isItDamaged === true ? `brown` : `gray`;
   }
   player.board[next].tile.style[`background-color`] = `red`;
-  return next;  
+  return next;
 };
+
 const deployShip = (player, shipId, comp) => {
-  if(checkIfDeployable(player, shipId)){
+
+  if (checkIfDeployable(player, shipId)) {
     player.ships[shipId].status = 'deployed';
     player.ships[shipId].components = comp
       .map((c, i) => {
@@ -217,7 +241,7 @@ const deployShip = (player, shipId, comp) => {
         player.board[c].componentIndex = i;
         player.board[c].tile.style[`background-color`] = `brown`;
         return c;
-      })
+      });
     // Always start vertically
     direction = true;
     deployingShips(player);
@@ -226,6 +250,7 @@ const deployShip = (player, shipId, comp) => {
   }
 };
 const changeDirection = (player, shipId, comp) => {
+
   direction = !direction;
   player.ships[shipId].components = comp
     .map(c => {
@@ -233,7 +258,7 @@ const changeDirection = (player, shipId, comp) => {
       // a potential boundary issue, but makes game quality worse?
       const next = `${c[1]}${c[0]}`;
       const curr = `${c[0]}${c[1]}`;
-      if(player.board[curr].occupiedBy){
+      if (player.board[curr].occupiedBy) {
         player.board[curr].tile.style[`background-color`] = `brown`;
       } else {
         player.board[curr].tile.style[`background-color`] = `white`;
@@ -242,50 +267,57 @@ const changeDirection = (player, shipId, comp) => {
       return next;
     });
 };
+
+// Checks if block can be moved
 const checkIfMoveable = (ship, keyCode) => {
+  
   const c = ship.components; // components
   const head = [c[0][0], c[0][1]]; // x, y
   const tail = `${c[c.length - 1][0]}${c[c.length - 1][1]}`;
-  // Checks if the ship can be moved
-  let moveAble = true;
 
-  if(keyCode === 87){
+  if (keyCode === 87) {
     // y of head cannot be 0, same case for 9 and all directions
-    moveAble = head[1] != 0; // type coersion
-  } else if(keyCode === 65){
-    moveAble = head[0] != 0;
-  } else if(keyCode === 83){
-    moveAble = tail[1] != 9;
-  } else if(keyCode === 68){
-    moveAble = tail[0] != 9;
+    return head[1] !== '0';
+  } else if (keyCode === 65) {
+    return head[0] !== '0';
+  } else if (keyCode === 83) {
+    return tail[1] !== '9';
+  } else if (keyCode === 68) {
+    return tail[0] !== '9';
   }
-  return moveAble;
 };
+
 const checkIfDeployable = (player, shipId) => {
   // If there is a true in any tile the current ship is currently hovering then you cannot deploy
-  const checkIfAvailable = player.ships[shipId].components.map((c) => {
-    if(player.board[c].occupiedBy) return false;
-      return true;
-  })
-  return !checkIfAvailable.includes(false)
+  const checkIfAvailable = player.ships[shipId].components.map(c => {
+    if (player.board[c].occupiedBy) {
+      return false;
+    }
+    return true;
+  });
+  return !checkIfAvailable.includes(false);
 };
+
 const deployingShips = (player) => {
 
   const battleShipKeys = Object.keys(player.ships);
-
-  for(let i = 0; i < battleShipKeys.length; i ++){
-    if(player.ships[battleShipKeys[i]].status === 'inactive'){
+  
+  for (let i = 0; i < battleShipKeys.length; i ++) {
+    
+    if (player.ships[battleShipKeys[i]].status === 'inactive') {
       player.ships[battleShipKeys[i]].components.forEach(c => {
         player.board[c].tile.style[`background-color`] = `black`;
-      })
+      });
       shipId = player.ships[battleShipKeys[i]].id;
       break;
     }
-    if(i === battleShipKeys.length - 1){
+
+    if (i === battleShipKeys.length - 1) {
       player.deployedAllShips = true;
-      if(playerOne.deployedAllShips && playerTwo.deployedAllShips){
+      readyTheField(player);
+      if (playerOne.deployedAllShips && playerTwo.deployedAllShips) {
         gameStarted = true;
-        readyTheField(player, player.nextPlayer);
+        playGame(currentPlayer);
       } else {
         currentPlayer = player.nextPlayer;
         shipId = battleShipId[0];
@@ -293,52 +325,63 @@ const deployingShips = (player) => {
       }
     }
   }
-  // Check if all players have deployed their ships
-  /* console.table(playerOne.board)
-  console.table(playerOne.ships)
-  console.table(playerTwo.board)
-  console.table(playerTwo.ships)
-  console.log(playerOne.id, 'has deployed all ships:', playerOne.deployedAllShips)
-  console.log(playerTwo.id, 'has deployed all ships:', playerTwo.deployedAllShips)
-  console.log('CurrentPlayer',currentPlayer.id, 'Deploying', shipId, 'Game started:', gameStarted);
-  */
-}
+};
+
 const readyTheField = (player) => {
   Object.keys(player.board).forEach(key => {
-    playerOne.board[key].tile.style[`background-color`] = `blue`;
-    playerTwo.board[key].tile.style[`background-color`] = `blue`;
-  })
-  playGame(player);
+    player.board[key].tile.style[`background-color`] = `gray`;
+  });
 };
+
 // Playing phase
 const playGame = (currentPlayer) => {
   currentPlayer.board[crossHair.components[0]].tile.style[`background-color`] = `red`;
 };
-const checkEndConditions = () => {
-  return false;
-};
+
 const moveCrossHair = (player, keyCode) => {
-  
-  if(keyCode !== 13 && keyCode !== 70){
+  if (keyCode !== 13 && keyCode !== 70) {
     //If the crosshair is at the boundaries
-    if(!checkIfMoveable(crossHair, keyCode)) return;
-      console.log(crossHair, crossHair.components)
-      crossHair.components[0] = changeTile(crossHair.components[0], keyCode, player);
-  } else if(keyCode === 13){
-    if(player.board[crossHair.components[0]].occupiedBy){
+    if (!checkIfMoveable(crossHair, keyCode)) {
+      return;
+    }
+    crossHair.components[0] = changeTile(crossHair.components[0], keyCode, player);
+  } else if (keyCode === 13 && !player.board[crossHair.components[0]].shotAt) {
+    player.board[crossHair.components[0]].shotAt = true;
+    if (player.board[crossHair.components[0]].occupiedBy) {
+
       const shipId = player.board[crossHair.components[0]].occupiedBy;
-      const shipComponentIndex = player.board[crossHair.components[0]].componentIndex;
+      
       player.board[crossHair.components[0]].componentIndex = true;
       player.ships[shipId].status = `damaged`;
-      console.log('Hit:', shipId, shipComponentIndex);
+    
+      if (player.ships[shipId].length > 0) {
+        player.ships[shipId].length -= 1;
+        if (checkIfAllShipsSunk(player.ships)) {
+          gameEnd = true;
+          console.log(player.nextPlayer.id, 'WON!');
+          return;
+        }
+      }
+
+      console.log(player.nextPlayer.id, 'hits on:', player.board[crossHair.components[0]].id);
       player.board[crossHair.components[0]].tile.style[`background-color`] = `brown`;
+
       currentPlayer = player.nextPlayer;
     } else {
       player.board[crossHair.components[0]].tile.style[`background-color`] = `blue`;
       currentPlayer = player.nextPlayer;
-      console.log('Miss');
+      console.log(player.id, 'misses on', player.board[crossHair.components[0]].id);
     }
   }
+
+  playGame(currentPlayer);
+
+};
+
+const checkIfAllShipsSunk = (ships) => {
+  const shipsSunk = Object.keys(ships).filter(key => ships[key].length === 0);
+  console.log('Sunken ships of:', currentPlayer.id, shipsSunk);
+  return shipsSunk.length === 5;
 };
 // Resetting the game
 const resetGame = () => {
@@ -347,33 +390,39 @@ const resetGame = () => {
   playerTwo.deployedAllShips = false;
 
   gameStarted = false;
+  gameEnd = false;
   shipId = battleShipId[0];
-  direction = true; 
-
-  for(key in playerOne.board){
+  direction = true;
+  
+  for (key in playerOne.board) {
     const p1Tile = playerOne.board[key];
     const p2Tile = playerTwo.board[key];
     p1Tile.tile.style[`background-color`] = `white`;
+    playerOne.board[key].shotAt = false;
     p1Tile.componentIndex = false;
     p1Tile.occupiedBy = false;
     p2Tile.tile.style[`background-color`] = `white`;
+    playerTwo.board[key].shotAt = false;
     p2Tile.componentIndex = false;
     p2Tile.occupiedBy = false;
   }
 
-  for(key in playerOne.ships){
+  for (key in playerOne.ships) {
     const p1Ship = playerOne.ships[key];
     const p2Ship = playerTwo.ships[key];
     p1Ship.components = p1Ship.components.map((c, i) => `0${i}`);
     p1Ship.status = 'inactive';
+    p1Ship.length = p1Ship.components.length;
     p2Ship.components = p2Ship.components.map((c, i) => `0${i}`);
     p2Ship.status = 'inactive';
+    p2Ship.length = p2Ship.components.length;
   }
 
-  currentPlayer = playerOne;
+  currentPlayer = Math.round(Math.random()) === 0 ? playerOne : playerTwo;
   
   deployingShips(currentPlayer);
 };
+
 // Create the grids
 createGrid(playerOneBoard, playerOne);
 createGrid(playerTwoBoard, playerTwo);
@@ -383,18 +432,15 @@ coordinateContext(columns, rows, letters);
 battleShipFactory(playerOne);
 battleShipFactory(playerTwo);
 
+console.log(currentPlayer.id, 'is first');
 deployingShips(currentPlayer);
 
 document.addEventListener('keydown', (e) => {
-  if(validKeyCodes[e.keyCode] && !gameStarted){
+  if (validKeyCodes[e.keyCode] && !gameStarted && !gameEnd) {
     moveShip(currentPlayer, shipId, e.keyCode);
-  }
-  if(validKeyCodes[e.keyCode] && gameStarted){
+  } else if (validKeyCodes[e.keyCode] && gameStarted && !gameEnd) {
     moveCrossHair(currentPlayer, e.keyCode);
-  }
-  if(e.keyCode === 82){
+  } else if (e.keyCode === 82) {
     resetGame();
   }
 });
-
-// resetButton.addEventListener('click', () => resetGame());
